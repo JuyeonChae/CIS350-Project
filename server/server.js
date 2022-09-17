a38676dabbb01684ded2a33c8123d4f733626fcb
JuyeonChae <jchae1126@gmail.com>
2022-09-17 19:16:01 -0400

    more structure

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 /**  a very fundamental code for http://localhost:3000 **/
 // use the built-in http module
+// require() is used to load and cache js modules
+// used when loading a local, relative js module into a Node.js app
 var http = require('http');
 
 // createServer is a method
@@ -8,9 +10,7 @@ var server = http.createServer(function(req, res) {
     res.end("Hello world\n");
 });
 
-server.listen(3000, function() {
+server.listen(3000, '0.0.0.0', function() {
     console.log('server is running at 3000')
 });
 // and type "node server" on terminal
-
-console.log(__filename)
