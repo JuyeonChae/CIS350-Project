da91c5c9f6143ea3606ee606d2c1b9fb8db5c2d0
JuyeonChae <jchae1126@gmail.com>
2022-09-19 10:46:36 -0400

    my dumb mistake

--- server/server.js
+++ server/server.js
@@ -2,15 +2,16 @@
 // use the built-in http module
 // require() is used to load and cache js modules
 // used when loading a local, relative js module into a Node.js app
-var http = require('http');
+const http = require('http');
 
 // createServer is a method
-var server = http.createServer(function(req, res) {
+const server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-type" : "text/plain"});
-    res.end("Hello world\n");
+    res.end("I made a server and this is it!!!!!! ;)\n");
 });
 
 server.listen(3000, '0.0.0.0', function() {
-    console.log('server is running at 3000')
+    console.log('server is so hard to make')
 });
+
 // and type "node server" on terminal
