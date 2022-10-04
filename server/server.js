508c3b5665c72695e77099710b561db4cfef85b9
JuyeonChae <jchae1126@gmail.com>
2022-10-03 20:27:20 -0400

    mongoDB Connected

--- server/server.js
+++ server/server.js
@@ -10,8 +10,17 @@ const server = http.createServer(function(req, res) {
     res.end("I made a server and this is it!!!!!! ;)\n");
 });
 
-server.listen(3000, '0.0.0.0', function() {
-    console.log('server is so hard to make')
+server.listen(3000, function() {
+    console.log('server is so hard to make');
 });
 
 // and type "node server" on terminal
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://jchae1126:<password>@cis350project.7liavoi.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
